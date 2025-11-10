e745a3c9c5ece904ea7c86266e699a5feb86edf9
FilippChup <filippartrider@gmail.com>
2025-11-10 11:33:42 +0100

    idk

--- obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+599c37e053ecbc0092a3e08f034d6991ba297d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409aa7fde5dc56b874b738f55bac0aeda414a250")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
